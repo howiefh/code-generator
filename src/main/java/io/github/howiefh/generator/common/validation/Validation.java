5fdb3574e9a9ca14fe772b5c64e42140a26436ff
Feng Hao <howiefh@gmail.com>
2016-05-23 21:07:57 +0800

    验证使用cglib替代jdk反射

--- src/main/java/io/github/howiefh/generator/common/validation/Validation.java
+++ src/main/java/io/github/howiefh/generator/common/validation/Validation.java
@@ -3,19 +3,21 @@ package io.github.howiefh.generator.common.validation;
 import io.github.howiefh.generator.common.exception.ValidationException;
 import io.github.howiefh.generator.common.util.CollectionUtils;
 import io.github.howiefh.generator.common.util.StringUtils;
+import net.sf.cglib.reflect.FastClass;
+import net.sf.cglib.reflect.FastMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.beans.IntrospectionException;
-import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 /**
+ * 反射采用了cglib类库，类似的有<a href="https://github.com/EsotericSoftware/reflectasm/tree/master/test/com/esotericsoftware/reflectasm/benchmark">reflectasm</a>
+ * ，性能和cglib差不多，可能还稍微好一些。
  * @author fenghao on 2016/5/21
  * @version 1.0
  * @since 1.0
@@ -25,7 +27,7 @@ public class Validation {
 
     private static final String GET = "get";
     /** 缓存get方法 */
-    private static Map<String, Method> getMethods = new HashMap<String, Method>();
+    private static Map<String, FastMethod> methods = new HashMap<String, FastMethod>();
 
     private Set<String> fields = new HashSet<String>();
     /** 验证器 */
@@ -72,10 +74,9 @@ public class Validation {
     }
     private static void cacheMethod(Class clazz, String field) throws IntrospectionException {
         String key = key(GET, clazz, field);
-        PropertyDescriptor pd = new PropertyDescriptor(field, clazz);
-        //获得读方法
-        Method getMethod = pd.getReadMethod();
-        getMethods.put(key, getMethod);
+        FastClass fastClass = FastClass.create(clazz);
+        FastMethod fastMethod = fastClass.getMethod(GET + StringUtils.capitalize(field), null);
+        methods.put(key, fastMethod);
     }
 
     /**
@@ -95,14 +96,12 @@ public class Validation {
         }
         for (String field : fields) {
             String key = key(GET, clazz, field);
-            Method getMethod = getMethods.get(key);
+            FastMethod getMethod = methods.get(key);
             try {
                 Object o = getMethod.invoke(obj, null);
                 if (!(isValid = validator.validate(o))){
                     throw new ValidationException(clazz + "." + field + " is fields！");
                 }
-            } catch (IllegalAccessException e) {
-                LOGGER.warn("{} can not access, E:{}", key, e.getMessage());
             } catch (InvocationTargetException e) {
                 LOGGER.warn("{} invoke error, E:{}", key, e.getMessage());
             }
